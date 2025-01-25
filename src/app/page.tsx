5db14319bbaff3cffa523a015b3fc092ff19ffdf
Techmeng Aing <aingtechmeng@gmail.com>
2025-01-25 06:16:49 -0500

    added drizzleadapter auth for google's auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,22 @@ import { database } from "@/db/database";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
 import { revalidatePath } from "next/cache";
-
+import { SignOut } from "@/components/sign-out";
+import { SignIn } from "@/components/sign-in";
+import { auth } from "@/auth";
 
 export default async function HomePage() {
+  const session = await auth();
+
+
   const bids = await database.query.bids.findMany();
 
   return (
-    <main className="">
+    <main className="container mx-auto py-12">
+      {session ? <SignOut /> : <SignIn />}
+      {session?.user?.name}
+
+
       <form
          action={async (formData: FormData) => {
           "use server";
